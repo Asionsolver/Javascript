e0b33cbc65c8296c43d5e7f01a34f449a74138cb
Asionsolver <asionsolver274@gmail.com>
2023-07-11 17:29:04 +0600

    add new file name like TraverseDOMElements.js file

---
+++ DOM/TraverseDOMElements.js
@@ -0,0 +1,26 @@
+// ! Traversing the DOM Elements
+
+// let list = document.getElementById('list')
+// console.log(list)
+
+// let parent = list.parentElement  // ? show parent element
+// console.log(parent)
+
+// let children = list.children
+// console.log(children)
+
+// let previousSibling = list.previousSibling // ? return node element.Not use
+// console.log(previousSibling)
+// let previousElementSiblings = list.previousElementSibling // ? return node element.Use
+// console.log(previousElementSiblings)
+// let nextElementSiblings = list.nextElementSibling // ? return node element.Use
+// console.log(nextElementSiblings)
+
+// let li = document.querySelector('li')
+
+// console.log(li.nextElementSibling)
+
+// console.log(list.firstChild) // ? return first child node that means text node
+
+// console.log(list.firstElementChild) //  ? return first element child node that means first html element
+// console.log(list.lastElementChild) // ?  return first element child node that means first html element
