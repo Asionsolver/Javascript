cd8d547e7242f99a989a9da7246d38b8fdeb230d
Asionsolver <asionsolver274@gmail.com>
2023-07-11 17:32:48 +0600

    add new file name like DOMSelector.js file

--- app.js
+++ app.js
@@ -1,35 +1,3 @@
-// let title = document.getElementById('title')  //select by id
-// console.log(title)
-
-// let paragraph =document.getElementsByClassName('lead') //select by class
-// console.log(paragraph)
-
-// let list = document.getElementsByTagName('li')  //select by tag
-// console.log(list)
-
-// let listItemOne = document.getElementsByName('list-item-one') //select by name
-// console.log(listItemOne)
-
-// let title = document.querySelector('#title') //select by id
-// console.log(title)
-
-// let paragraph = document.querySelector('.lead') //select by class
-// console.log(paragraph)
-
-// let paragraphs = document.querySelectorAll('.lead') //select by class
-// console.log(paragraphs)
-
-// let list =document.querySelectorAll('li') //select by tag
-// console.log(list)
-
-// let listItemOne =document.querySelectorAll('[name^="list-item-one"]') //select by name
-// console.log(listItemOne)
-
-
-
-
-
-
 // ! How to Loop Throw HTML Collection in Javascript
 
 // let listItem = document.getElementsByTagName('li'); // ? returns node list
