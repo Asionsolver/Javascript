b39a749151278b9a65cc022686a5bf953d5eb68b
Asionsolver <asionsolver274@gmail.com>
2023-07-11 17:31:37 +0600

    add new file name like GetElementsVsQuerySelector.js file

--- app.js
+++ app.js
@@ -26,20 +26,6 @@
 // console.log(listItemOne)
 
 
-// ! Get Elements Method vs Query Selector in Javascript
-
-
-// let li1 = document.getElementsByTagName('li')  // ? use element node list
-// console.log(li1)
-// console.log(li1.constructor.name)
-
-// let li2 = document.querySelectorAll('li')  // ? use node list [Element,Comment,Text]
-// console.log(li2)
-// console.log(li2.constructor.name)
-
-// let ul =document.querySelector('ul')
-// console.log(ul.children)
-// console.log(ul.childNodes) // ? returns all child nodes
 
 
 
