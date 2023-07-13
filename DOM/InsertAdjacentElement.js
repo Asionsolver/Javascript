f6ef24abb8ea08e6d991096018086342fdb60841
Asionsolver <asionsolver274@gmail.com>
2023-07-14 00:57:31 +0600

    add new file name like InsertAdjacentElement.js

--- DOM/InsertAdjacentElement.js
+++ DOM/InsertAdjacentElement.js
@@ -1,32 +1,32 @@
-// let li = document.createElement('li') ;
-
-// li.className = 'list-group-item' ;
-// li.setAttribute('title' , 'I am a title') ;
-// li.innerHTML = 'Four' ;
-// list = document.getElementById('list') ;
-// list.appendChild(li) ;
 
 
 let li = createElement('li' , 'list-group-item' , 'Four') ;
+
+let list = document.getElementById('list') ;
+list.appendChild(li) ;
+// li.appendChild(paragraph1) ;
+// console.log(li) ;
+
 
 let paragraph1 = createElement('p' , 'text-success' , 'I am a paragraph') ;
 let paragraph2 = createElement('p' , 'text-success' , 'I am a paragraph 2') ;
 
 let div = createElement('div' , 'container' , '') ;
 append(div , [paragraph1 , paragraph2]) ;
-console.log(div) ;
+// console.log(div) ;
 
-let containers = document.getElementById('topic') ;
+// list.insertAdjacentElement('beforebegin' , div) ;
+// list.insertAdjacentElement('afterbegin' , div) ;
+// list.insertAdjacentElement('afterend' , div) ;
+list.insertAdjacentElement('beforeend' , div) ;
+
+// let containers = document.getElementById('topic') ;
 // console.log(containers) ;
-containers.appendChild(div) ; 
+// containers.appendChild(div) ; 
 
 
 
-let list = document.getElementById('list') ;
-list.appendChild(li) ;
-// li.appendChild(paragraph1) ;
 
-// console.log(li) ;
 
 
 // this function will create an element with the given tag name , class name and innerHTML
@@ -44,5 +44,3 @@ function append(parent , children) {
         parent.appendChild(child) ;
     });
 }
-
-
