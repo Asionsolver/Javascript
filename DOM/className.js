79f9bb145005a2b81f5840f75d48e144b5d143f0
Asionsolver <asionsolver274@gmail.com>
2023-07-13 11:00:37 +0600

    add new file name like className.js file

---
+++ DOM/className.js
@@ -0,0 +1,27 @@
+/**
+className: The className property represents the class attribute of an element as a string. It allows you to get or set the class names assigned to an element. If an element has multiple classes, they are separated by spaces within the string.
+*/
+
+const element = document.getElementById('myDiv');
+// console.log(element);
+
+// console.log(element.className); // "red bold"
+// console.log(element);
+
+
+
+
+// element.className = 'blue';
+// console.log(element.className); // "blue"
+// console.log(element);
+
+
+
+// element.className = 'white';
+// console.log(element.className); // "white"
+// console.log(element);
+
+
+element.className = 'purple semi-bold';
+console.log(element.className); // "purple semi-bold"
+console.log(element);
