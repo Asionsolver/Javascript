f5789d25ec8d3951615e9ecc53488a1130ceb641
Asionsolver <asionsolver274@gmail.com>
2023-07-29 22:59:47 +0600

    add new file name like selector.js file

---
+++ DOM/selectors.js
@@ -0,0 +1,24 @@
+// let title = document.getElementById("title");
+// let title = document.getElementById("title").className;
+// let title = document.getElementById("title").id;
+// let title = document.getElementById("title").innerHTML;
+// let title = document.getElementById("title").textContent;
+// let title = document.getElementById("title").getAttribute("class");
+// let title = document.getElementById("title").getAttribute("id");
+// let title = document.getElementById("title").setAttribute("class", "heading test");
+// console.log(title);
+
+// title.style.background = "white";
+// title.style.color = "black";
+// title.style.padding = "20px";
+// title.style.display = "inline-block";
+// title.style.border = "4px solid aqua";
+// title.style.borderRadius = "5px";
+
+
+// console.log(title.innerText);
+// console.log(title.textContent);
+
+let title = document.querySelector('h4');
+console.log(title);
+let input = document.querySelector('input[type=password]');
